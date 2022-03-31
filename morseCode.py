300a2b8ded8998369414a21641490068533ac304
Prajwal Singh <prajwalrishabh@gmail.com>
2022-03-31 19:31:48 +0530

    Fixed the Bug with findContour

--- morseCode.py
+++ morseCode.py
@@ -57,8 +57,11 @@ class MorseImageDecoder(object):
 		# cv2.imshow('Erode Image',erodeImage)	
 		# cv2.waitKey(0)
 		# cv2.destroyAllWindows()
-
-		contour = cv2.findContours(erodeImage.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+		
+		# https://stackoverflow.com/a/58290380 [Thanks to Ricardo Abe for the answer, opencv modified the findContour fn]
+		# Thanks to @mazharmachu28 for finding the bug [old findContour method return values]
+		# old: contour = cv2.findContours(erodeImage.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+		contour = cv2.findContours(erodeImage.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
 		copyImage = np.zeros((self.image.shape[0],self.image.shape[1],3),dtype='uint8')
 		# cv2.drawContours(copyImage,contour,-1,(0,0,255),1)
 		# cv2.imshow('Con Image',copyImage)	
